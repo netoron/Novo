101d76f3e8ad7ed3ca025f57b4b2d28f0f4ce6d7
erick <erickblz@outlook.com>
2020-02-21 22:45:53 -0300

    adicionando Programa

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voando igual uma maracuja");
+            Console.WriteLine("Voando igual um caju");
+        }
+    }
+}
